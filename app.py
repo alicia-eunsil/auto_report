0acb5c34a551174727adf217d44a923d7591a808
alicia-eunsil <gjf.eunsil@gmail.com>
2026-02-19 09:21:40 +0900

    style: keep worsening chart as line and remove detail flow table

--- app.py
+++ app.py
@@ -516,11 +516,7 @@ with status_tab:
     if indicator_worse_monthly.empty:
         st.info("월간 악화건수 그래프를 표시할 데이터가 없습니다.")
     else:
-        if len(indicator_worse_monthly.index) >= 2:
-            st.line_chart(indicator_worse_monthly)
-        else:
-            st.bar_chart(indicator_worse_monthly)
-            st.caption("월 데이터가 1개라 선 연결이 불가하여 막대로 표시합니다.")
+        st.line_chart(indicator_worse_monthly)
 
     if indicator_flow.empty:
         st.info("신호 비교 데이터(prev_2m/prev_1m/current)가 없어 변화 분석이 불가능합니다.")
@@ -538,7 +534,6 @@ with status_tab:
         with f4:
             st.metric("재개선 지역", int(direction_summary["reimprove_regions"]))
             st.caption(format_region_names(direction_summary["reimprove_names"]))
-        render_centered_table(indicator_flow)
 
     st.markdown("### 현재 신호 분포")
     level_opt = st.selectbox("권역 선택", ["전국", "시도", "경기 시군"], index=1, key="status_level_opt")
